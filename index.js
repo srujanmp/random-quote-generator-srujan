4adaa58c63c96178bef3a6a2371a87fc91ee3939
Srujan M Padmashali <srujanmpadmashali@gmail.com>
2024-06-04 13:05:31 +0530

    first

---
+++ index.js
@@ -0,0 +1,51 @@
+                                               
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+require('dotenv').config();
+let bodyParser = require('body-parser');
+app.use(express.static(__dirname+'/src'))  // for stylesheets to work
+mongoose.connect(process.env.MONGO_URI);
+let max=34;let min=1;
+let quoteSchema = new mongoose.Schema({   //created quote schema
+    no: Number,
+    quote: String
+});
+
+let quote = mongoose.model('quote', quoteSchema);  //created quote model
+app.use(bodyParser.urlencoded({ extended: false })); //to get input from html body
+
+app.get('/',async(req,res)=>{
+    let randno=Math.floor(Math.random() * (max - min + 1)) + min;
+    let getquote=await quote.findOne({ no: randno});
+    res.send(getquote.quote);
+})
+
+
+
+
+
+
+
+
+
+app.get('/post', (req, res) => {
+    res.sendFile(__dirname + '/postquote.html');  //serves index.html at root
+})
+app.post('/postquote',async(req,res)=>{
+    let quoteexist = await quote.findOne({ quote: req.body.quote });
+    if(quoteexist){
+        return res.send('quote aldready exists');
+    }
+    if(req.body.pin!=process.env.PIN){
+        return res.sendFile(__dirname+'/index.html');
+    }
+    let newquote = new quote({ no: req.body.no, quote: req.body.quote });
+    newquote.save().then((result) => {console.log("QUOTE SUBMITTED");}).catch((err) => {console.log(err);});
+    
+    res.sendFile(__dirname + '/postquote.html');
+})
+
+
+app.listen(8000);
+
